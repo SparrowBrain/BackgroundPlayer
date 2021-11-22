7d66137427facd881efbb9c693e9c8906d641f47
SparrowBrain <qwxmail@gmail.com>
2021-11-22 17:38:42 +0200

    Using caliburn.micro conductors

--- BackgroundPlayer.Wpf/SkinPoolViewModel.cs
+++ BackgroundPlayer.Wpf/SkinPoolViewModel.cs
@@ -4,14 +4,11 @@ using System.Linq;
 
 namespace BackgroundPlayer.Wpf
 {
-    public class SkinPoolViewModel : Screen
+    public class SkinPoolViewModel : Conductor<SkinDetailsViewModel>.Collection.OneActive
     {
         public SkinPoolViewModel(SkinLoader skinLoader)
         {
-            Skins = new BindableCollection<SkinDetailsViewModel>();
-            Skins.AddRange(skinLoader.LoadSkins().Select(x => new SkinDetailsViewModel { Name = x.Name, Duration = x.Duration, OffsetMonth = x.StartOffset.Month, OffsetDay = x.StartOffset.Day, OffsetHour = x.StartOffset.Hour }));
+            Items.AddRange(skinLoader.LoadSkins().Select(x => new SkinDetailsViewModel { Name = x.Name, Duration = x.Duration, OffsetMonth = x.StartOffset.Month, OffsetDay = x.StartOffset.Day, OffsetHour = x.StartOffset.Hour }));
         }
-
-        public BindableCollection<SkinDetailsViewModel> Skins { get; set; }
     }
 }
