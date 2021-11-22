c17e2d3d0abe80a0568c9de6ac76221ecc2bc18f
SparrowBrain <qwxmail@gmail.com>
2021-11-22 11:04:02 +0200

    Fixed skin pool to show skins again

--- BackgroundPlayer.Wpf/SkinPoolViewModel.cs
+++ BackgroundPlayer.Wpf/SkinPoolViewModel.cs
@@ -1,13 +1,15 @@
-﻿using System.Linq;
+﻿using BackgroundPlayer.Configuration;
 using Caliburn.Micro;
+using System.Linq;
 
 namespace BackgroundPlayer.Wpf
 {
     public class SkinPoolViewModel : Screen
     {
-        public SkinPoolViewModel()
+        public SkinPoolViewModel(SkinLoader skinLoader)
         {
             Skins = new BindableCollection<SkinDetailsViewModel>();
+            Skins.AddRange(skinLoader.LoadSkins().Select(x => new SkinDetailsViewModel { Name = x.Name, Duration = x.Duration, OffsetMonth = x.StartOffset.Month, OffsetDay = x.StartOffset.Day, OffsetHour = x.StartOffset.Hour }));
         }
 
         public BindableCollection<SkinDetailsViewModel> Skins { get; set; }
