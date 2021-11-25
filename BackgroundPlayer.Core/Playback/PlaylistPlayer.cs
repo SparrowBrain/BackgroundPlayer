6642c1ee6b0b600188777afa67ce169c3faef618
SparrowBrain <qwxmail@gmail.com>
2021-11-25 18:46:55 +0200

    Skin is now randomly picked at the startup and doesn't change

--- BackgroundPlayer.Core/Playback/PlaylistPlayer.cs
+++ BackgroundPlayer.Core/Playback/PlaylistPlayer.cs
@@ -1,38 +1,26 @@
-﻿using System;
+﻿using BackgroundPlayer.Core.Model;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using BackgroundPlayer.Core.Configuration;
-using BackgroundPlayer.Core.Model;
 
 namespace BackgroundPlayer.Core.Playback
 {
     public class PlaylistPlayer
     {
         private readonly IPlayer _player;
-        private readonly PlaybackState _playbackState;
 
-        public PlaylistPlayer(IPlayer player, PlaybackState playbackState)
+        public PlaylistPlayer(IPlayer player)
         {
             _player = player;
-            _playbackState = playbackState;
         }
 
         public async Task Play(List<Skin> skins, CancellationToken cancellationToken)
         {
-            if(_playbackState!=null)
-            {
-                var skin = skins.FirstOrDefault(x => x.Name == _playbackState.SkinName);
-                if (skin != null)
-                {
-                    await _player.PlaySkin(skin, _playbackState.PlaybackStarted, cancellationToken);
-                }
-            }
             var random = new Random();
+            var skin = skins[random.Next(skins.Count)];
             while (true)
             {
-                var skin = skins[random.Next(skins.Count)];
                 await _player.PlaySkin(skin, cancellationToken);
                 cancellationToken.ThrowIfCancellationRequested();
             }
